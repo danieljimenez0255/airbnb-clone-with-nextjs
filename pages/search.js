1759e3116eb98767c2d8c5706e174411efdd153d
Daniel <42664403+danieljimenez0255@users.noreply.github.com>
2021-09-01 19:55:02 -0700

    main pages

---
+++ pages/search.js
@@ -0,0 +1,22 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const Search = () => {
+  return (
+    <div>
+      <Header />
+      <main className="flex">
+        <section>
+          <p className="text-xs">300+ Stays for 5 number of guests</p>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Mars</h1>
+          <div className="hidden lg:inline-flex">
+            <p className="button">Cancellation Flexibility</p>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Search;
