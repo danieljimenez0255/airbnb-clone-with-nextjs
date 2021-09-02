768a5fe53520cee484d48d0560d576c064543de1
Daniel <42664403+danieljimenez0255@users.noreply.github.com>
2021-09-02 10:04:00 -0700

    implemented home/search routing

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,12 +10,14 @@ import { useState } from "react";
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { DateRangePicker } from "react-date-range";
+import { useRouter } from "next/dist/client/router";
 
-const Header = () => {
+const Header = ({ placeholder = "Start your search..." }) => {
   const [searchInput, setSearchInput] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [guests, setGuests] = useState(1);
+  const router = useRouter();
 
   const selectionRange = {
     startDate: startDate,
@@ -28,9 +30,24 @@ const Header = () => {
     setEndDate(ranges.selection.endDate);
   };
 
+  const search = () => {
+    router.push({
+      pathname: "/search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        guests,
+      },
+    });
+  };
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
-      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+      <div
+        onClick={() => router.push("/")}
+        className="relative flex items-center h-10 cursor-pointer my-auto"
+      >
         <Image
           src="https://links.papareact.com/qd3"
           alt="airbnb nextjs logo"
@@ -42,7 +59,7 @@ const Header = () => {
       <div className="flex items-center cursor-pointer md:border-2 rounded-full py-2 md:shadow-sm">
         <input
           type="text"
-          placeholder="Start your search"
+          placeholder={placeholder}
           className="pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400"
           value={searchInput}
           onChange={(event) => setSearchInput(event.target.value)}
@@ -86,7 +103,11 @@ const Header = () => {
             >
               Cancel
             </button>
-            <button type="button" className="flex-grow text-red-400">
+            <button
+              onClick={search}
+              type="button"
+              className="flex-grow text-red-400"
+            >
               Search
             </button>
           </div>
