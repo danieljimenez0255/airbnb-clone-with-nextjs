bf75aebfa46561dfdf8703110e504cb08f8adf59
Daniel <42664403+danieljimenez0255@users.noreply.github.com>
2021-09-30 03:05:38 -0700

    area codes for phones input api

---
+++ pages/api/areacodes.js
@@ -0,0 +1,972 @@
+export default function handler(req, res) {
+  res.send([
+    {
+      code: "(+93)",
+      location: "Afghanistan",
+    },
+    {
+      code: "(+358)",
+      location: "Åland Islands",
+    },
+    {
+      code: "(+355)",
+      location: "Albania",
+    },
+    {
+      code: "(+213)",
+      location: "Algeria",
+    },
+    {
+      code: "(+1)",
+      location: "American Samoa",
+    },
+    {
+      code: "(+376)",
+      location: "Andorra",
+    },
+    {
+      code: "(+244)",
+      location: "Angola",
+    },
+    {
+      code: "(+1)",
+      location: "Anguilla",
+    },
+    {
+      code: "(+1)",
+      location: "Antigua &amp; Barbuda",
+    },
+    {
+      code: "(+54)",
+      location: "Argentina",
+    },
+    {
+      code: "(+374)",
+      location: "Armenia",
+    },
+    {
+      code: "(+297)",
+      location: "Aruba",
+    },
+    {
+      code: "(+61)",
+      location: "Australia",
+    },
+    {
+      code: "(+43)",
+      location: "Austria",
+    },
+    {
+      code: "(+994)",
+      location: "Azerbaijan",
+    },
+    {
+      code: "(+1)",
+      location: "Bahamas",
+    },
+    {
+      code: "(+973)",
+      location: "Bahrain",
+    },
+    {
+      code: "(+880)",
+      location: "Bangladesh",
+    },
+    {
+      code: "(+1)",
+      location: "Barbados",
+    },
+    {
+      code: "(+375)",
+      location: "Belarus",
+    },
+    {
+      code: "(+32)",
+      location: "Belgium",
+    },
+    {
+      code: "(+501)",
+      location: "Belize",
+    },
+    {
+      code: "(+229)",
+      location: "Benin",
+    },
+    {
+      code: "(+1)",
+      location: "Bermuda",
+    },
+    {
+      code: "(+975)",
+      location: "Bhutan",
+    },
+    {
+      code: "(+591)",
+      location: "Bolivia",
+    },
+    {
+      code: "(+599)",
+      location: "Bonaire, Sint Eustatius and Saba",
+    },
+    {
+      code: "(+387)",
+      location: "Bosnia &amp; Herzegovina",
+    },
+    {
+      code: "(+267)",
+      location: "Botswana",
+    },
+    {
+      code: "(+55)",
+      location: "Brazil",
+    },
+    {
+      code: "(+246)",
+      location: "British Indian Ocean Territory",
+    },
+    {
+      code: "(+1)",
+      location: "British Virgin Islands",
+    },
+    {
+      code: "(+673)",
+      location: "Brunei",
+    },
+    {
+      code: "(+359)",
+      location: "Bulgaria",
+    },
+    {
+      code: "(+226)",
+      location: "Burkina Faso",
+    },
+    {
+      code: "(+257)",
+      location: "Burundi",
+    },
+    {
+      code: "(+855)",
+      location: "Cambodia",
+    },
+    {
+      code: "(+237)",
+      location: "Cameroon",
+    },
+    {
+      code: "(+1)",
+      location: "Canada",
+    },
+    {
+      code: "(+238)",
+      location: "Cape Verde",
+    },
+    {
+      code: "(+1)",
+      location: "Cayman Islands",
+    },
+    {
+      code: "(+236)",
+      location: "Central African Republic",
+    },
+    {
+      code: "(+235)",
+      location: "Chad",
+    },
+    {
+      code: "(+56)",
+      location: "Chile",
+    },
+    {
+      code: "(+86)",
+      location: "China",
+    },
+    {
+      code: "(+61)",
+      location: "Christmas Island",
+    },
+    {
+      code: "(Keeling) Islands (+61)",
+      location: "Cocos",
+    },
+    {
+      code: "(+57)",
+      location: "Colombia",
+    },
+    {
+      code: "(+269)",
+      location: "Comoros",
+    },
+    {
+      code: "(+242)",
+      location: "Congo",
+    },
+    {
+      code: "(+682)",
+      location: "Cook Islands",
+    },
+    {
+      code: "(+506)",
+      location: "Costa Rica",
+    },
+    {
+      code: "(+225)",
+      location: "Côte d’Ivoire",
+    },
+    {
+      code: "(+385)",
+      location: "Croatia",
+    },
+    {
+      code: "(+53)",
+      location: "Cuba",
+    },
+    {
+      code: "(+599)",
+      location: "Curaçao",
+    },
+    {
+      code: "(+357)",
+      location: "Cyprus",
+    },
+    {
+      code: "(+420)",
+      location: "Czechia",
+    },
+    {
+      code: "(+243)",
+      location: "Democratic Republic of the Congo",
+    },
+    {
+      code: "(+45)",
+      location: "Denmark",
+    },
+    {
+      code: "(+253)",
+      location: "Djibouti",
+    },
+    {
+      code: "(+1)",
+      location: "Dominica",
+    },
+    {
+      code: "(+1)",
+      location: "Dominican Republic",
+    },
+    {
+      code: "(+593)",
+      location: "Ecuador",
+    },
+    {
+      code: "(+20)",
+      location: "Egypt",
+    },
+    {
+      code: "(+503)",
+      location: "El Salvador",
+    },
+    {
+      code: "(+240)",
+      location: "Equatorial Guinea",
+    },
+    {
+      code: "(+291)",
+      location: "Eritrea",
+    },
+    {
+      code: "(+372)",
+      location: "Estonia",
+    },
+    {
+      code: "(+268)",
+      location: "Eswatini",
+    },
+    {
+      code: "(+251)",
+      location: "Ethiopia",
+    },
+    {
+      code: "(Islas Malvinas) (+500)",
+      location: "Falkland Islands",
+    },
+    {
+      code: "(+298)",
+      location: "Faroe Islands",
+    },
+    {
+      code: "(+679)",
+      location: "Fiji",
+    },
+    {
+      code: "(+358)",
+      location: "Finland",
+    },
+    {
+      code: "(+33)",
+      location: "France",
+    },
+    {
+      code: "(+594)",
+      location: "French Guiana",
+    },
+    {
+      code: "(+689)",
+      location: "French Polynesia",
+    },
+    {
+      code: "(+241)",
+      location: "Gabon",
+    },
+    {
+      code: "(+220)",
+      location: "Gambia",
+    },
+    {
+      code: "(+995)",
+      location: "Georgia",
+    },
+    {
+      code: "(+49)",
+      location: "Germany",
+    },
+    {
+      code: "(+233)",
+      location: "Ghana",
+    },
+    {
+      code: "(+350)",
+      location: "Gibraltar",
+    },
+    {
+      code: "(+30)",
+      location: "Greece",
+    },
+    {
+      code: "(+299)",
+      location: "Greenland",
+    },
+    {
+      code: "(+1)",
+      location: "Grenada",
+    },
+    {
+      code: "(+590)",
+      location: "Guadeloupe",
+    },
+    {
+      code: "(+1)",
+      location: "Guam",
+    },
+    {
+      code: "(+502)",
+      location: "Guatemala",
+    },
+    {
+      code: "(+44)",
+      location: "Guernsey",
+    },
+    {
+      code: "(+224)",
+      location: "Guinea",
+    },
+    {
+      code: "(+245)",
+      location: "Guinea-Bissau",
+    },
+    {
+      code: "(+592)",
+      location: "Guyana",
+    },
+    {
+      code: "(+509)",
+      location: "Haiti",
+    },
+    {
+      code: "(+504)",
+      location: "Honduras",
+    },
+    {
+      code: "(+852)",
+      location: "Hong Kong",
+    },
+    {
+      code: "(+36)",
+      location: "Hungary",
+    },
+    {
+      code: "(+354)",
+      location: "Iceland",
+    },
+    {
+      code: "(+91)",
+      location: "India",
+    },
+    {
+      code: "(+62)",
+      location: "Indonesia",
+    },
+    {
+      code: "(+964)",
+      location: "Iraq",
+    },
+    {
+      code: "(+353)",
+      location: "Ireland",
+    },
+    {
+      code: "(+44)",
+      location: "Isle of Man",
+    },
+    {
+      code: "(+972)",
+      location: "Israel",
+    },
+    {
+      code: "(+39)",
+      location: "Italy",
+    },
+    {
+      code: "(+1)",
+      location: "Jamaica",
+    },
+    {
+      code: "(+81)",
+      location: "Japan",
+    },
+    {
+      code: "(+44)",
+      location: "Jersey",
+    },
+    {
+      code: "(+962)",
+      location: "Jordan",
+    },
+    {
+      code: "(+7)",
+      location: "Kazakhstan",
+    },
+    {
+      code: "(+254)",
+      location: "Kenya",
+    },
+    {
+      code: "(+686)",
+      location: "Kiribati",
+    },
+    {
+      code: "(+383)",
+      location: "Kosovo",
+    },
+    {
+      code: "(+965)",
+      location: "Kuwait",
+    },
+    {
+      code: "(+996)",
+      location: "Kyrgyzstan",
+    },
+    {
+      code: "(+856)",
+      location: "Laos",
+    },
+    {
+      code: "(+371)",
+      location: "Latvia",
+    },
+    {
+      code: "(+961)",
+      location: "Lebanon",
+    },
+    {
+      code: "(+266)",
+      location: "Lesotho",
+    },
+    {
+      code: "(+231)",
+      location: "Liberia",
+    },
+    {
+      code: "(+218)",
+      location: "Libya",
+    },
+    {
+      code: "(+423)",
+      location: "Liechtenstein",
+    },
+    {
+      code: "(+370)",
+      location: "Lithuania",
+    },
+    {
+      code: "(+352)",
+      location: "Luxembourg",
+    },
+    {
+      code: "(+853)",
+      location: "Macau",
+    },
+    {
+      code: "(+389)",
+      location: "North Macedonia",
+    },
+    {
+      code: "(+261)",
+      location: "Madagascar",
+    },
+    {
+      code: "(+265)",
+      location: "Malawi",
+    },
+    {
+      code: "(+60)",
+      location: "Malaysia",
+    },
+    {
+      code: "(+960)",
+      location: "Maldives",
+    },
+    {
+      code: "(+223)",
+      location: "Mali",
+    },
+    {
+      code: "(+356)",
+      location: "Malta",
+    },
+    {
+      code: "(+692)",
+      location: "Marshall Islands",
+    },
+    {
+      code: "(+596)",
+      location: "Martinique",
+    },
+    {
+      code: "(+222)",
+      location: "Mauritania",
+    },
+    {
+      code: "(+230)",
+      location: "Mauritius",
+    },
+    {
+      code: "(+262)",
+      location: "Mayotte",
+    },
+    {
+      code: "(+52)",
+      location: "Mexico",
+    },
+    {
+      code: "(+691)",
+      location: "Micronesia",
+    },
+    {
+      code: "(+373)",
+      location: "Moldova",
+    },
+    {
+      code: "(+377)",
+      location: "Monaco",
+    },
+    {
+      code: "(+976)",
+      location: "Mongolia",
+    },
+    {
+      code: "(+382)",
+      location: "Montenegro",
+    },
+    {
+      code: "(+1)",
+      location: "Montserrat",
+    },
+    {
+      code: "(+212)",
+      location: "Morocco",
+    },
+    {
+      code: "(+258)",
+      location: "Mozambique",
+    },
+    {
+      code: "(+95)",
+      location: "Myanmar",
+    },
+    {
+      code: "(+264)",
+      location: "Namibia",
+    },
+    {
+      code: "(+674)",
+      location: "Nauru",
+    },
+    {
+      code: "(+977)",
+      location: "Nepal",
+    },
+    {
+      code: "(+31)",
+      location: "Netherlands",
+    },
+    {
+      code: "(+687)",
+      location: "New Caledonia",
+    },
+    {
+      code: "(+64)",
+      location: "New Zealand",
+    },
+    {
+      code: "(+505)",
+      location: "Nicaragua",
+    },
+    {
+      code: "(+227)",
+      location: "Niger",
+    },
+    {
+      code: "(+234)",
+      location: "Nigeria",
+    },
+    {
+      code: "(+683)",
+      location: "Niue",
+    },
+    {
+      code: "(+672)",
+      location: "Norfolk Island",
+    },
+    {
+      code: "(+1)",
+      location: "Northern Mariana Islands",
+    },
+    {
+      code: "(+47)",
+      location: "Norway",
+    },
+    {
+      code: "(+968)",
+      location: "Oman",
+    },
+    {
+      code: "(+92)",
+      location: "Pakistan",
+    },
+    {
+      code: "(+680)",
+      location: "Palau",
+    },
+    {
+      code: "(+970)",
+      location: "Palestinian Territories",
+    },
+    {
+      code: "(+507)",
+      location: "Panama",
+    },
+    {
+      code: "(+675)",
+      location: "Papua New Guinea",
+    },
+    {
+      code: "(+595)",
+      location: "Paraguay",
+    },
+    {
+      code: "(+51)",
+      location: "Peru",
+    },
+    {
+      code: "(+63)",
+      location: "Philippines",
+    },
+    {
+      code: "(+64)",
+      location: "Pitcairn Islands",
+    },
+    {
+      code: "(+48)",
+      location: "Poland",
+    },
+    {
+      code: "(+351)",
+      location: "Portugal",
+    },
+    {
+      code: "(+1)",
+      location: "Puerto Rico",
+    },
+    {
+      code: "(+974)",
+      location: "Qatar",
+    },
+    {
+      code: "(+262)",
+      location: "Réunion",
+    },
+    {
+      code: "(+40)",
+      location: "Romania",
+    },
+    {
+      code: "(+7)",
+      location: "Russia",
+    },
+    {
+      code: "(+250)",
+      location: "Rwanda",
+    },
+    {
+      code: "(+685)",
+      location: "Samoa",
+    },
+    {
+      code: "(+378)",
+      location: "San Marino",
+    },
+    {
+      code: "(+239)",
+      location: "São Tomé &amp; Príncipe",
+    },
+    {
+      code: "(+966)",
+      location: "Saudi Arabia",
+    },
+    {
+      code: "(+221)",
+      location: "Senegal",
+    },
+    {
+      code: "(+381)",
+      location: "Serbia",
+    },
+    {
+      code: "(+248)",
+      location: "Seychelles",
+    },
+    {
+      code: "(+232)",
+      location: "Sierra Leone",
+    },
+    {
+      code: "(+65)",
+      location: "Singapore",
+    },
+    {
+      code: "(+1)",
+      location: "Sint Maarten",
+    },
+    {
+      code: "(+421)",
+      location: "Slovakia",
+    },
+    {
+      code: "(+386)",
+      location: "Slovenia",
+    },
+    {
+      code: "(+677)",
+      location: "Solomon Islands",
+    },
+    {
+      code: "(+252)",
+      location: "Somalia",
+    },
+    {
+      code: "(+27)",
+      location: "South Africa",
+    },
+    {
+      code: "(+500)",
+      location: "South Georgia &amp; South Sandwich Islands",
+    },
+    {
+      code: "(+82)",
+      location: "South Korea",
+    },
+    {
+      code: "(+211)",
+      location: "South Sudan",
+    },
+    {
+      code: "(+34)",
+      location: "Spain",
+    },
+    {
+      code: "(+94)",
+      location: "Sri Lanka",
+    },
+    {
+      code: "(+590)",
+      location: "St. Barthélemy",
+    },
+    {
+      code: "(+290)",
+      location: "St. Helena",
+    },
+    {
+      code: "(+1)",
+      location: "St. Kitts &amp; Nevis",
+    },
+    {
+      code: "(+1)",
+      location: "St. Lucia",
+    },
+    {
+      code: "(+590)",
+      location: "St. Martin",
+    },
+    {
+      code: "(+508)",
+      location: "St. Pierre &amp; Miquelon",
+    },
+    {
+      code: "(+1)",
+      location: "St. Vincent &amp; Grenadines",
+    },
+    {
+      code: "(+249)",
+      location: "Sudan",
+    },
+    {
+      code: "(+597)",
+      location: "Suriname",
+    },
+    {
+      code: "(+47)",
+      location: "Svalbard &amp; Jan Mayen",
+    },
+    {
+      code: "(+46)",
+      location: "Sweden",
+    },
+    {
+      code: "(+41)",
+      location: "Switzerland",
+    },
+    {
+      code: "(+886)",
+      location: "Taiwan",
+    },
+    {
+      code: "(+992)",
+      location: "Tajikistan",
+    },
+    {
+      code: "(+255)",
+      location: "Tanzania",
+    },
+    {
+      code: "(+66)",
+      location: "Thailand",
+    },
+    {
+      code: "(+670)",
+      location: "Timor-Leste",
+    },
+    {
+      code: "(+228)",
+      location: "Togo",
+    },
+    {
+      code: "(+690)",
+      location: "Tokelau",
+    },
+    {
+      code: "(+676)",
+      location: "Tonga",
+    },
+    {
+      code: "(+1)",
+      location: "Trinidad &amp; Tobago",
+    },
+    {
+      code: "(+216)",
+      location: "Tunisia",
+    },
+    {
+      code: "(+90)",
+      location: "Turkey",
+    },
+    {
+      code: "(+993)",
+      location: "Turkmenistan",
+    },
+    {
+      code: "(+1)",
+      location: "Turks &amp; Caicos Islands",
+    },
+    {
+      code: "(+688)",
+      location: "Tuvalu",
+    },
+    {
+      code: "(+1)",
+      location: "U.S. Virgin Islands",
+    },
+    {
+      code: "(+256)",
+      location: "Uganda",
+    },
+    {
+      code: "(+380)",
+      location: "Ukraine",
+    },
+    {
+      code: "(+971)",
+      location: "United Arab Emirates",
+    },
+    {
+      code: "(+44)",
+      location: "United Kingdom",
+    },
+    {
+      code: "(+1)",
+      location: "United States",
+    },
+    {
+      code: "(+598)",
+      location: "Uruguay",
+    },
+    {
+      code: "(+998)",
+      location: "Uzbekistan",
+    },
+    {
+      code: "(+678)",
+      location: "Vanuatu",
+    },
+    {
+      code: "(+379)",
+      location: "Vatican City",
+    },
+    {
+      code: "(+58)",
+      location: "Venezuela",
+    },
+    {
+      code: "(+84)",
+      location: "Vietnam",
+    },
+    {
+      code: "(+681)",
+      location: "Wallis &amp; Futuna",
+    },
+    {
+      code: "(+212)",
+      location: "Western Sahara",
+    },
+    {
+      code: "(+967)",
+      location: "Yemen",
+    },
+    {
+      code: "(+260)",
+      location: "Zambia",
+    },
+    {
+      code: "(+263)",
+      location: "Zimbabwe",
+    },
+  ]);
+}
