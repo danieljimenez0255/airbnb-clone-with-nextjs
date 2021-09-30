fd6ea47e3897e4bce5d4422b66980699cf2e885e
Daniel <42664403+danieljimenez0255@users.noreply.github.com>
2021-09-30 02:58:39 -0700

    project configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   images: {
-    domains: ["links.papareact.com"],
+    domains: [
+      "links.papareact.com",
+      "thumbnails.trvl-media.com",
+      "exp.cdn-hotels.com",
+      "platform-lookaside.fbsbx.com",
+      "lh3.googleusercontent.com",
+      "i.scdn.co",
+      "avatars.githubusercontent.com",
+    ],
   },
 };
